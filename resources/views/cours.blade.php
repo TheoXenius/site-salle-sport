b1a32d031cc8072aa5f7beb532569d62865ebd94
[TheoXenius] <[theo.bedu@icloud.com]>
2023-12-07 14:17:16 +0100

    page de cours:

--- resources/views/cours.blade.php
+++ resources/views/cours.blade.php
@@ -4,4 +4,19 @@
 
 @section('content')
 <h2>Cours</h2>
+<li>
+    lundi 19h à 21h zumba
+</li>
+<li>
+    mardi 19h30 à 21h30 pilate
+</li>
+<li>
+    jeudi 20h à 22h boxe anglaise
+</li>
+<li>
+    vendredi 20h à 22h hiit
+</li>
+<li>
+    samedi 9h à 11h yoga
+</li>
 @endsection
