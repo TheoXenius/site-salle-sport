89f31f6974b53abf4d8e3243b470fa236415fe6f
[TheoXenius] <[theo.bedu@icloud.com]>
2023-12-07 14:02:45 +0100

    page d'accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -6,4 +6,5 @@
 <h2>Accueil</h2>
 <img src="img/risen-wang-20jX9b35r_M-unsplash (1).jpg" alt="salle 1">
 <img src="img/sam-moghadam-khamseh-0sa8D74iodI-unsplash (1).jpg" alt="salle 2">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero vel quos earum id itaque quisquam. Fugit, animi itaque debitis molestias soluta quas, architecto autem, reprehenderit nam commodi vero enim voluptate?</p>
 @endsection
