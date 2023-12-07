ba2f939810a406683a27a54159dd0eeccd302dc0
[TheoXenius] <[theo.bedu@icloud.com]>
2023-12-07 14:24:19 +0100

    page mentions legales:

--- resources/views/mentions-legales.blade.php
+++ resources/views/mentions-legales.blade.php
@@ -4,4 +4,5 @@
 
 @section('content')
 <h2>Mention Légales</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate earum eveniet laudantium quia, consequatur doloremque facere ex dolores corporis! Rerum, voluptates velit? Rerum perspiciatis corrupti maxime aperiam explicabo dolorem asperiores!</p>
 @endsection
