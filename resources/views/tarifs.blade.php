50b8bb2ff027aabefb07a5ebe891dc74aa18c7f6
[TheoXenius] <[theo.bedu@icloud.com]>
2023-12-07 14:23:06 +0100

    page tarifs:

--- resources/views/tarifs.blade.php
+++ resources/views/tarifs.blade.php
@@ -4,4 +4,19 @@
 
 @section('content')
 <h2>Tarifs</h2>
+<li>
+    adhésion annuelle à l'associtation : 15 eur
+</li>
+<li>
+    la séance : 10 eur
+</li>
+<li>
+    le carnet de 10 séances : 90 eur
+</li>
+<li>
+    abonnement mensuel illimité sans cours : 30 eur
+</li>
+<li>
+    abonnement mensuel illimité avec cours : 60 eur
+</li>
 @endsection
