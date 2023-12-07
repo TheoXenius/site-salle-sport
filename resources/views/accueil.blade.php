08a1c35c2f4bd9206837c982476b39e836c16934
[TheoXenius] <[theo.bedu@icloud.com]>
2023-12-07 12:49:37 +0100

    Etape 11:

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -4,4 +4,6 @@
 
 @section('content')
 <h2>Accueil</h2>
+<img src="img/" alt="public/img/risen-wang-20jX9b35r_M-unsplash.jpg">
+<img src="img/" alt="public/img/sam-moghadam-khamseh-0sa8D74iodI-unsplash.jpg">
 @endsection
